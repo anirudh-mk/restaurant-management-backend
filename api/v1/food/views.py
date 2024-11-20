ccfbd786b6fee2992bfa83a00f1b8ce12450e40f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-11-20 22:33:27 +0530

    feat(response changed in foods)

--- api/v1/food/views.py
+++ api/v1/food/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 
 from api.v1.food.serializer import CategorySerializer
 from db.models import Food, Category
+from utils.response import CustomResponse
 
 
 class CategoryAPI(APIView):
@@ -11,32 +12,34 @@ class CategoryAPI(APIView):
             category_instance = Category.objects.filter(
                 food_category_query__restaurant__id=resturent_id
             ).distinct()
+
             serializer = CategorySerializer(category_instance, many=True)
-            return Response(serializer.data)
+
+            return CustomResponse(response=serializer.data).get_success_response()
 
         category_instance = Category.objects.all()
         serializer = CategorySerializer(category_instance, many=True)
-        return Response(serializer.data)
+        return CustomResponse(response=serializer.data).get_success_response()
 
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response("Category created successfully")
-        return Response(serializer.errors)
+            return CustomResponse(general_message="Category created successfully").get_success_response()
+        return CustomResponse(response=serializer.errors).get_failure_response()
 
     def patch(self, request, pk):
         country_instance = Category.objects.filter(pk=pk).first()
         if not country_instance:
-            return Response("Category not found")
+            return CustomResponse(general_message="Category not found").get_failure_response()
         serializer = CategorySerializer(country_instance, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response("Category edited successfully")
-        return Response(serializer.errors())
+            return CustomResponse(general_message="Category edited successfully").get_success_response()
+        return CustomResponse(response=serializer.errors).get_failure_response()
 
     def delete(self, request, pk):
         if category_instance := Category.objects.filter(pk=pk).first():
             category_instance.delete()
-            return Response("Category deleted successfully")
-        return Response("Category not found")
+            return CustomResponse(general_message="Category deleted successfully").get_success_response()
+        return CustomResponse(general_message="Category not found").get_failure_response()
