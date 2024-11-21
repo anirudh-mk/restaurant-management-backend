538622ba5687dbad54c61e07441b14e35b7fcd4a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-11-21 07:05:07 +0530

    feat(restauarnt feat and delete)

--- api/v1/restaurant/views.py
+++ api/v1/restaurant/views.py
@@ -22,3 +22,19 @@ class RestaurantAPI(APIView):
             serializer.save()
             return CustomResponse(general_message='Restaurant created successfully').get_success_response()
         return CustomResponse(response=serializer.errors).get_failure_response()
+
+    def patch(self, request, pk):
+        restaurant_instance = Restaurant.objects.filter(pk=pk).first()
+        if not restaurant_instance:
+            return CustomResponse(general_message='Restaurant not found').get_failure_response()
+        serializer = RestaurantSerializer(restaurant_instance, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return CustomResponse(general_message='Restauarnt edited successfully').get_success_response()
+        return CustomResponse(response=serializer.errors).get_failure_response()
+
+    def delete(self, request, pk):
+        if restaurant_instance := Restaurant.objects.filter(pk=pk).first():
+            restaurant_instance.delete()
+            return CustomResponse(general_message='Restaurant deleted successfully').get_success_response()
+        return CustomResponse(general_message='Restaurant not found').get_failure_response()
